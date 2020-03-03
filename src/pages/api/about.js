734022c4f396ec22e574ca9e5b4f0cf9a0e388d9
henry <ihenvyr@gmail.com>
2020-03-03 10:33:33 +0800

    /api/about json api routes added

---
+++ src/pages/api/about.js
@@ -0,0 +1,8 @@
+export default (req, res) => {
+  res.json({
+    name: "Henry Cruz",
+    email: "ihenvyr@gmail.com",
+    description: "Front-end Developer",
+    skills: ["next.js", "react.js", "express.js", "node.js", "javascript", "ruby on rails", "css", "html"],
+  });
+};
